cb4933cc18b5206557e0a7f73e3b108534bc3d88
Li Zhao <iloahz@gmail.com>
2023-02-04 22:32:09 +0800

    remove signal files

--- signals/db-signals.go
+++
@@ -1,6 +0,0 @@
-package signals
-
-type DBSignals struct {
-	Type      string `json:"type" bson:"type"`
-	Timestamp int64  `json:"timestamp" bson:"timestamp"`
-}
--- signals/db-trigger.go
+++
@@ -1,6 +0,0 @@
-package signals
-
-type DBTrigger struct {
-	SignalType string `json:"signal_type" bson:"signal_type"`
-	Action     string `json:"action" bson:"action"`
-}
